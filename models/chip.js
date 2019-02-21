ca45865825fe73dce62d98dce1a13e14b748b586
Arrhe <arrhenr@gmail.com>
2019-02-21 14:22:46 -0500

    ah

--- models/chip.js
+++ models/chip.js
@@ -17,7 +17,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false
         },
         imageURL: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false
         }
     })
